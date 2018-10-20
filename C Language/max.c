08f71fcc4148f5c55d64258aafbe0f4d1c3f7e1f
raghavshrm987 <18ucc156@lnmiit.ac.in>
2018-10-20 17:14:39 +0530

    add max in clanguage

---
+++ C Language/max.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("maximu of two number\n");
+printf("enter a number =:");
+scanf("%d",&a);
+printf("enter a number =:");
+scanf("%d",&b);
+if(a>b)
+printf("%d is max",a);
+else
+printf("%d is max",b);
+return 0;
+}
